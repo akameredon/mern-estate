c45771552897bebbe6d7bba6f0f243f5b5c3adf4
akadonye <droagrict@gmail.com>
2024-08-12 15:04:03 +0100

    create a middleware and a function to handle possible errors

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,9 +1,5 @@
 export const test = (req, res) => {
     res.json({
-<<<<<<< HEAD
         message: 'Api route is working!',
-=======
-        message: 'Hello World!',
->>>>>>> f6f345555cc0390b0d78c64762f14d7ec0c30d09
     });
 };
